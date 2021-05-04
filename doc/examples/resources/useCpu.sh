cf94b65f7b3c0f4211f8b0ad3a031f35e51c7639
Jan Mrázek <email@honzamrazek.cz>
2021-05-04 10:01:48 +0200

    Fix meaning of CPU and memory

--- doc/examples/resources/useCpu.sh
+++ doc/examples/resources/useCpu.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Just compute SQRT for $1 seconds on $2 threads
-stress --vm 1 --vm-bytes $2 --timeout $1
+stress --cpu $2 --timeout $1
 
 # Output artefact for surveyor:
-echo {\"bytes\": $2, \"timeout\": $1} > /artefact/results.json
+echo {\"cpu\": $2, \"timeout\": $1} > /artefact/results.json
--- doc/examples/resources/useMemory.sh
+++ doc/examples/resources/useMemory.sh
@@ -3,7 +3,8 @@
 set -e
 
 # Just compute SQRT for $1 seconds on $2 threads
-stress --cpu $2 --timeout $1
+stress --vm 1 --vm-bytes $2 --timeout $1
 
 # Output artefact for surveyor:
-echo {\"cpu\": $2, \"timeout\": $1} > /artefact/results.json
+echo {\"bytes\": $2, \"timeout\": $1} > /artefact/results.json
+
